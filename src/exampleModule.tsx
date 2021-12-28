f9ddfb7f8599de13780d7b105c7b9c84fbcfe5db
Brennan Fife <brenfife@gmail.com>
2021-12-28 01:35:07 -0600

    Remove styled comp

--- src/exampleModule.tsx
+++ src/exampleModule.tsx
@@ -1,39 +1,9 @@
-import React from 'react';
-import styled, {keyframes}  from 'styled-components';
+import React from "react";
 
-const rotate = keyframes`
-    from{
-        transform:rotate(0deg);
-    }
-    to{
-        transform:rotate(360deg)
-    }
-`
-
-const StyledCenter = styled.div`
-    display:flex;
-    flex-direction:center;
-    justify-content:center;
-    align-items:'center';
-`
-
-const StyledRotatingText = styled.div`
-    color:palevioletred;
-    display: inline-block;;
-    animation: ${rotate} 2s linear infinite;
-    padding: 2rem 1rem;
-    font-size:1.2rem;
-    margin:auto;
-`
-
-interface Props{
-    text:string
-}
-
-export const RotatingText = ({text}:Props)=>{
-    return(
-        <StyledCenter>
-            <StyledRotatingText>{text}</StyledRotatingText>
-        </StyledCenter>             
-    )
-}
+export const RotatingText = ({ text }: { text: string }) => {
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  );
+};
