6c4904b4ef1933a22dd29cf8d6857deeed1e8d2c
Brennan Fife <brenfife@gmail.com>
2021-12-28 14:30:06 -0600

    -

---
+++ dist/testingtestingtesting-package.cjs.development.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+var RotatingText = function RotatingText(_ref) {
+  var text = _ref.text;
+  return React.createElement("div", null, React.createElement("h1", null, text));
+};
+
+exports.RotatingText = RotatingText;
+//# sourceMappingURL=testingtestingtesting-package.cjs.development.js.map
---
+++ dist/testingtestingtesting-package.esm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+var RotatingText = function RotatingText(_ref) {
+  var text = _ref.text;
+  return React.createElement("div", null, React.createElement("h1", null, text));
+};
+
+export { RotatingText };
+//# sourceMappingURL=testingtestingtesting-package.esm.js.map
